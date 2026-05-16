cc49e0b91db70085f77bcd3a1e52074d63eaf173
Patryk Kowalczyk <108098577+GregoriiK@users.noreply.github.com>
2026-05-16 13:28:53 +0200

    Interactable shader control

--- Assets/Scripts/RayCastInteractable.cs
+++ Assets/Scripts/RayCastInteractable.cs
@@ -20,35 +20,41 @@ public class RayCastInteractable : MonoBehaviour
         RaycastHit hit;
         Physics.Raycast(gameObject.transform.position, transform.TransformDirection(Vector3.forward), out hit, rayRange);
 
-        if (hit.collider != null )
+        if (hit.collider == null)
         {
-            if(lookingAtObject == null)
-            {
-                lookingAtObject = hit.collider.gameObject;
-            }
-            else
-            {
-                if (lookingAtObject == hit.collider.gameObject) 
-                {
-                    Debug.Log("the same");
-                }
-                else
-                {
+            return;
+        }
 
-                }
-            }
-            if (hit.collider.CompareTag("Interactable"))
-            {
-                Material[] mats = hit.collider.GetComponent<MeshRenderer>().materials;
-                foreach (Material mat in mats) 
+        if(lookingAtObject == null)
+        {
+            lookingAtObject = hit.collider.gameObject;
+        }
+        else
+        {
+            if (lookingAtObject != hit.collider.gameObject) 
+            {   
+                foreach (Material mat in lookingAtObject.GetComponent<MeshRenderer>().materials)
                 {
                     if (mat.HasFloat("_IsActive"))
                     {
-                        mat.SetInt("_IsActive", 1);
+                        mat.SetInt("_IsActive", 0);
                     }
                 }
             }
+            if (!hit.collider.CompareTag("Interactable"))
+            {
+                return;
+            }
+            Material[] mats = hit.collider.GetComponent<MeshRenderer>().materials;
+            foreach (Material mat in mats) 
+            {
+                if (mat.HasFloat("_IsActive"))
+                {
+                    mat.SetInt("_IsActive", 1);
+                }            
+            }
+
         }
-        lookingAtObject = hit.collider.gameObject;
+        lookingAtObject = hit.collider.gameObject;        
     }
 }
